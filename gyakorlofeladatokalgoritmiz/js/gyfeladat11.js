4536e20b5045ac5816697adc3baf2243a4d65228
juli59 <molnarjuli59@gmail.com>
2020-11-23 07:22:07 +0100

    gyakorlófeladata 11 algoritmizalas

---
+++ gyakorlofeladatokalgoritmiz/js/gyfeladat11.js
@@ -0,0 +1,18 @@
+function bubbleSort(arr) {
+    let swapped;
+    do {
+        swapped = false;
+        for (let i = 0; i < arr.length - 1; i += 1) {
+            if (arr[i] > arr[i + 1]) {
+                let temp = arr[i];
+                arr[i] = arr[i + 1];
+                arr[i + 1] = temp;
+                swapped = true;
+            }
+        }
+    } while (swapped)
+    return arr
+}
+
+const arr = [43.5, -5.67, 3.3, 9.9999, 67.1, 567.89];
+console.log(bubbleSort(arr));
